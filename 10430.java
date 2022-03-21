7d4904e4bb5c4ecbde7f586cbd693eb19743a0a3
ChoiMinJun <102012307+heugi04@users.noreply.github.com>
2022-03-21 15:18:02 +0900

    Create 10430.java

---
+++ 10430.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int first = s.nextInt();
+		int second = s.nextInt();
+		int third = s.nextInt();
+		
+		System.out.println((first + second)%third);
+		System.out.println(((first % third) + (second % third)) % third);
+		System.out.println((first * second) % third);
+		System.out.println(((first % third) * (second % third)) % third);
+	}
+
+}
