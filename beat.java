3e3a6c8823fb43ef816b09de0f60b85cf3222af5
ChoiMinJun <102012307+heugi04@users.noreply.github.com>
2022-03-23 17:44:47 +0900

    Create beat.java

---
+++ beat.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		
+		int x = s.nextInt();
+		
+		if (x % 2 != 0);
+			int q = (x / 2) + 1;
+		if (q % 2 != 0);
+			int w = (q / 2) + 1;
+		if (w % 2 != 0);
+			int e = (w / 2) + 1;
+		if (e % 2 != 0);
+			int r = (e / 2) + 1;
+		
+					
+		System.out.println();	
+	}
+
+}
